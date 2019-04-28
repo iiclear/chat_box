a7d3d3eff4ca740c027f6bca7e1b59020ce56380
iiclear <43413171+iiclear@users.noreply.github.com>
2019-04-28 16:30:47 +0800

    语料采集

---
+++ corpus/qa_spider.py
@@ -0,0 +1,17 @@
+
+# http://www.ccutu.com/wenwen/1/
+# 写一个爬虫类
+# 会在本文件夹生成以数字编号的txt文件，比如1.txt
+# 每个TXT文件里面有100对问答，比如：
+# 你好！
+# 你也好。
+# 这是一对，以'\n'为分隔符的问答
+
+#写一个文本处理的类
+# 能够把txt文件夹里的问答转为列表，如：
+# [
+#     'Hello, how are you?',
+#     'I am doing well.',
+#     'That is good to hear.',
+#     'Thank you'
+# ]
