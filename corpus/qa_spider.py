0e5269758e1689ac334a969de30706c06e0bd6f5
iiclear <43413171+iiclear@users.noreply.github.com>
2019-05-14 11:18:41 +0800

    Youtu爬虫

--- corpus/qa_spider.py
+++ corpus/qa_spider.py
@@ -1,17 +1,79 @@
 
-# http://www.ccutu.com/wenwen/1/
-# 写一个爬虫类
-# 会在本文件夹生成以数字编号的txt文件，比如1.txt
-# 每个TXT文件里面有100对问答，比如：
-# 你好！
-# 你也好。
-# 这是一对，以'\n'为分隔符的问答
+import requests
+import re
+import time
+from bs4 import BeautifulSoup
 
-#写一个文本处理的类
-# 能够把txt文件夹里的问答转为列表，如：
-# [
-#     'Hello, how are you?',
-#     'I am doing well.',
-#     'That is good to hear.',
-#     'Thank you'
-# ]
+class BS:
+    def __init__(self,url):
+        self.url =url
+    def analysis(self):
+        web_data = requests.get(self.url).content
+        soup = BeautifulSoup(web_data, 'lxml')
+        return soup
+
+
+class YouTuSpider:
+    def __init__(self):
+     self.url = 'http://www.ccutu.com/wenwen/'
+
+    # 爬取http://www.ccutu.com/wenwen/2/，http://www.ccutu.com/wenwen/3/
+    # 像这样的链接里面的问题标题的链接，
+    # 比如：http://www.ccutu.com/wenwen/answer35435.html
+    # 把这些链接存到一个列表urls里面，返回这个列表
+    def page_url(self):
+        urls = []
+        page = 1
+        while page <= 1112:
+            start_url = self.url + str(page) + '/'
+            page = page + 1
+            soup = BS(start_url).analysis()
+            items = soup.select('#form1 > div.wenda_cont > div._cont_left > ul > li > a')
+            # 'http://www.ccutu.com/wenwen/answer35452.html'
+            for item in items:
+                urls.append('http://www.ccutu.com' + item.get('href'))
+        # print(urls,len(urls))
+        return urls
+
+    # 参数是page_url()函数返回的列表，从中取出问答的链接，然后爬取问答。
+    # 把每一对问答存成字典，再把字典存到列表中，返回这个列表
+    # 可以参考我给你改的youtu()函数中字典的部分
+    def qa_data(self,urls):
+        item = {}
+        qa_list = []
+        for url in urls:
+            web_data = requests.get(url).content
+            soup = BeautifulSoup(web_data, 'lxml')
+            question = soup.select_one('body > div.wenda_cont > div.con_left > dl > h1').get_text().strip()
+            answer = ''.join(re.findall(r'<p>(.*?)</p>', str(
+                soup.select_one('body > div.wenda_cont > div.con_left > ul > li')))).replace('<br/>', '')
+            # answer = soup.select('body > div.wenda_cont > div.con_left > ul:nth-child(3) > li')
+
+            item = {
+                'question': question,
+                'answer': answer
+            }
+
+            qa_list.append(item)
+
+        return qa_list
+
+    def wirte_to_file(self,qa_list):
+        with open('qa.txt', 'w') as f:
+            for qa in qa_list:
+                f.write(qa['question'] + '\n' + qa['answer'] + '\n')
+        f.close()
+
+
+
+
+
+def main():
+    spider = YouTuSpider()
+    urls = spider.page_url()
+    qa_list = spider.qa_data(urls)
+    spider.wirte_to_file(qa_list)
+
+
+if __name__ == '__main__':
+    main()
